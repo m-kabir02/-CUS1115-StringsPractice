f46a6c5f8b819f97de976d7c1f68ba284aa1bb1e
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2022-10-08 22:31:21 -0400

    Add files via upload

---
+++ MyInterviewStrings/src/Interview.java
@@ -0,0 +1,52 @@
+/*
+Mosammad Kabir
+CUS 1115
+Professor DeBello
+February 4, 2021
+ */
+public class Interview {
+    public static void main(String[] args)
+    {
+        String first=("Mosammad");
+        String last=("Kabir");
+        String fullName=(first+" "+last);
+
+        String major=("Computer Science");
+        String minor=("No minor");
+        String type=("commuter");
+
+        char color;
+        color='c';
+        String c=("purple");
+
+        char sport;
+        sport='s';
+        String s="figure skating";
+
+        char food;
+        food='f';
+        String f="Chipotle";
+
+        String dreamVacay=("Santorini, Greece");
+
+        System.out.println("Hello, my name is"+" "+fullName);
+        System.out.println("I am majoring in"+" "+major);
+        System.out.println(minor);
+        System.out.println("I am a"+" "+type+" "+"student");
+        System.out.println("My favorite color is"+" "+c);
+        System.out.println("My favorite sport is"+" "+s);
+        System.out.println("My favorite food is"+" "+f);
+        System.out.println("My dream vacation is"+" "+dreamVacay);
+    }
+}
+
+/*
+ Hello, my name is Mosammad Kabir
+I am majoring in Computer Science
+No minor
+I am a commuter student
+My favorite color is purple
+My favorite sport is figure skating
+My favorite food is Chipotle
+My dream vacation is Santorini, Greece
+ */
